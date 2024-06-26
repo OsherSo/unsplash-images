94024f8770bee61807c7f39a8fdb2137fb66760a
Osher <solimani.osher@gmail.com>
2024-06-26 17:51:54 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Gallery, SearchForm, ThemeToggle } from "./components";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
